6b3c7f83c2927eeafc06bd5d099b649875c51592
juliparodi <julianparodi19@gmail.com>
2026-05-08 17:46:09 +0200

    feat: latest port changes

--- order-service/src/main/java/com/techmarket/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/techmarket/orderservice/config/WebClientConfig.java
@@ -3,14 +3,21 @@ package com.techmarket.orderservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        return WebClient
+                .builder()
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create().responseTimeout(Duration.ofMillis(2500))
+                ));
     }
 }

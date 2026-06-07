d105de565dea009cc892119779d6b731ca0c7646
juliparodi <julianparodi19@gmail.com>
2026-06-07 16:49:04 +0200

    fix: adding getter and setter

--- api-gateway/src/main/java/org/techmarket/config/RateLimitGatewayFilterFactory.java
+++ api-gateway/src/main/java/org/techmarket/config/RateLimitGatewayFilterFactory.java
@@ -145,7 +145,20 @@ public class RateLimitGatewayFilterFactory
             this.burstCapacity = burstCapacity;
         }
 
+        public int getReplenishRate() {
+            return replenishRate;
+        }
 
+        public void setReplenishRate(int replenishRate) {
+            this.replenishRate = replenishRate;
+        }
 
+        public int getBurstCapacity() {
+            return burstCapacity;
+        }
+
+        public void setBurstCapacity(int burstCapacity) {
+            this.burstCapacity = burstCapacity;
+        }
     }
 }

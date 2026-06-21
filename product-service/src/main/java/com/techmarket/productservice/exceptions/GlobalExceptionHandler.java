871a54cd9e2956088064d5bede67627d7f31c915
juliparodi <julianparodi19@gmail.com>
2026-06-21 13:55:48 +0200

    feat: adding logs in product exceptions

--- product-service/src/main/java/com/techmarket/productservice/exceptions/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/techmarket/productservice/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.techmarket.productservice.exceptions;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,25 +14,30 @@ import jakarta.validation.ValidationException;
 import static com.techmarket.productservice.exceptions.constants.ExceptionConstants.PROMOTION_ALREADY_EXIST;
 
 @RestControllerAdvice
+@Log4j2
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PromotionAlreadyExistException.class)
     public ResponseEntity<String> handleSuperHeroNotFoundException(PromotionAlreadyExistException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(PROMOTION_ALREADY_EXIST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("holi");
     }
 
     @ExceptionHandler({ValidationException.class, ConstraintViolationException.class})
     public ResponseEntity<String> handleValidationException(RuntimeException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class,})
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage());
         FieldError fieldError = ex.getBindingResult().getFieldErrors().get(0);
         String errorMessage = fieldError.getDefaultMessage();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
@@ -39,11 +45,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CategoryNotFoundException.class)
     public ResponseEntity<String> handleCategoryNotFoundException(CategoryNotFoundException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }

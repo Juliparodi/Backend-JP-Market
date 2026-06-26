424de093b88d01c06c85d84eb694362c5a1916ed
juliparodi <julianparodi19@gmail.com>
2026-06-26 14:16:39 +0200

    feat: ading cache to product

--- product-service/src/main/java/com/techmarket/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/techmarket/productservice/ProductServiceApplication.java
@@ -2,10 +2,8 @@ package com.techmarket.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {

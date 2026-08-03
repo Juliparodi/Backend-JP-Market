36811051a6d3ea22e1ba828b096281c34f779f07
juliparodi <julianparodi19@gmail.com>
2026-08-03 18:35:47 +0200

    fix: pubblic in the main class

--- ai-ops-agent/src/main/java/com/jpmarket/aiopsagent/AiOpsAgentApplication.java
+++ ai-ops-agent/src/main/java/com/jpmarket/aiopsagent/AiOpsAgentApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class AiOpsAgentApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(AiOpsAgentApplication.class, args);
     }
 
